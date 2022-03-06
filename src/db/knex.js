9648da33de2558a5ccd6ada6aee85afa1ccaaf3d
Mikhail Buinouskiy <verybadprogrammer@gmail.com>
2022-03-06 20:36:20 +0300

    Fix knex heroku ssl issue

--- src/db/knex.js
+++ src/db/knex.js
@@ -1,9 +1,14 @@
 import knex from "knex";
 import { attachPaginate } from "knex-paginate";
+import { parse } from "pg-connection-string";
+
+const pgconfig = parse(process.env.DATABASE_URL);
+
+pgconfig.ssl = { rejectUnauthorized: false };
 
 const instance = knex({
   client: "pg",
-  connection: process.env.DATABASE_URL,
+  connection: pgconfig,
   pool: { min: 0, max: 20 }
 });
 
