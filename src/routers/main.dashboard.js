f1f6ac3eb7a99223004f9c28492d3f7dce29f4b6
Mikhail Buinouskiy <verybadprogrammer@gmail.com>
2022-03-07 17:29:36 +0300

    Add excel report endpoint

--- src/routers/main.dashboard.js
+++ src/routers/main.dashboard.js
@@ -1,5 +1,6 @@
 import Router from "express-promise-router";
 import validator from "validator";
+import ExcelJS from "exceljs";
 import isAuth from "../middleware/auth.js";
 import knex from "../db/knex.js";
 import { log } from "../log.js";
@@ -56,8 +57,7 @@ router.post("/table/count", isAuth, async (req, res) => {
 router.post("/table", isAuth, async (req, res) => {
   const page = req.body.page ?? 1;
   const limit = req.body.limit ?? 50;
-  const orderBy =
-    req.body.orderBy && req.body.orderBy.toLowerCase() === "status" ? "status" : "created_at";
+  const orderBy = req.body.orderBy ? req.body.orderBy : "created_at";
   const orderDirection =
     req.body.orderDirection && req.body.orderDirection.toLowerCase() === "ascending"
       ? "asc"
@@ -114,5 +114,148 @@ router.patch("/table", isAuth, async (req, res) => {
   } catch (error) {
     log(req.ip, "sql", error, true);
     return res.status(500).end();
+  }
+});
+
+function padStr(i) {
+  return i < 10 ? `0${i}` : `${i}`;
+}
+
+function dateToStr(date) {
+  // prettier-ignore
+  return `${padStr(date.getFullYear())}-${padStr(date.getMonth())}-${padStr(date.getDate())} ${padStr(date.getHours())}:${padStr(date.getMinutes())}`;
+}
+
+function createTextRule(text, color) {
+  return {
+    type: "containsText",
+    operator: "containsText",
+    text,
+    style: {
+      fill: { type: "pattern", pattern: "solid", bgColor: { argb: color } },
+      font: {
+        italic: true,
+        color: { argb: "ffffff" }
+      }
+    }
+  };
+}
+
+router.post("/report", isAuth, async (req, res) => {
+  try {
+    const orderBy = req.body.orderBy ? req.body.orderBy : "created_at";
+    const orderDirection =
+      req.body.orderDirection && req.body.orderDirection.toLowerCase() === "ascending"
+        ? "asc"
+        : "desc";
+    const data = await knex("view_requests")
+      .select(
+        "created_at",
+        "done_at",
+        "status",
+        "technician",
+        "performed_works",
+        "cabinet",
+        "client",
+        "client_phone",
+        "defects"
+      )
+      .where((builder) => filterBuilder(builder, req.body.filters))
+      .orderBy(orderBy, orderDirection);
+    if (data.length === 0) {
+      return res.status(400).end();
+    }
+    const fileName = `Отчет ${dateToStr(new Date())}`;
+    const workbook = new ExcelJS.Workbook();
+    workbook.creator = "MTEC";
+    workbook.lastModifiedBy = "MTEC";
+    workbook.created = new Date();
+    workbook.modified = new Date();
+    const sheet = workbook.addWorksheet("Отчет", {
+      properties: {
+        tabColor: "6d81a2"
+      },
+      pageSetup: {
+        paperSize: 9,
+        orientation: "portrait"
+      },
+      headerFooter: {
+        firstHeader: `Отчет по заявкам на ремонт. &IОтчет был сгенерирован ${dateToStr(new Date())}`
+      }
+    });
+    sheet.columns = [
+      {
+        header: "Создано в",
+        key: "created_at",
+        width: 18,
+        style: { numFmt: "dd/mm/yyyy hh:MM:ss" }
+      },
+      {
+        header: "Выполнено в",
+        key: "done_at",
+        width: 18,
+        style: { numFmt: "dd/mm/yyyy hh:MM:ss" }
+      },
+      { header: "Статус", key: "status", width: 15 },
+      { header: "Мастер", key: "technician", width: 40 },
+      { header: "Проделанные работы", key: "performed_works", width: 60 },
+      { header: "Кабинет", key: "cabinet", width: 40 },
+      { header: "Заказчик", key: "client", width: 40 },
+      { header: "Телефон", key: "client_phone", width: 16 },
+      { header: "Неисправности", key: "defects", width: 60 }
+    ];
+    sheet.addConditionalFormatting({
+      ref: "C1:C9999",
+      rules: [
+        createTextRule("Выполнено", "67c23a"),
+        createTextRule("Просрочено", "f56c6c"),
+        createTextRule("Меньше часа", "e6a23c"),
+        createTextRule("Меньше недели", "909399"),
+        createTextRule("Больше недели", "909399")
+      ]
+    });
+    data.forEach((row) => {
+      let status = "";
+      switch (row.status) {
+        case "6:completed":
+          status = "Выполнено";
+          break;
+        case "1:expired":
+          status = "Просрочено";
+          break;
+        case "2:hour":
+          status = "Меньше часа";
+          break;
+        case "3:day":
+          status = "Меньше дня";
+          break;
+        case "4:week":
+          status = "Меньше недели";
+          break;
+        case "5:none":
+          status = "Больше недели";
+          break;
+        default:
+          status = "Неизвестно";
+      }
+      sheet.addRow({
+        created_at: new Date(row.created_at),
+        done_at: row.done_at ? new Date(row.done_at) : null,
+        status,
+        technician: row.technician ?? null,
+        performed_works: row.performed_works ? row.performed_works.join("; ") : null,
+        cabinet: row.cabinet,
+        client: row.client,
+        client_phone: row.client_phone ?? null,
+        defects: row.defects
+      });
+    });
+    res.set("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    res.set("Content-Disposition", `attachment; filename=${fileName}`);
+    await workbook.xlsx.write(res);
+    return res.status(200).end();
+  } catch (error) {
+    log(req.ip, "sql", error, true);
+    return res.status(400).end();
   }
 });
