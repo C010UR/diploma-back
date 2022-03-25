b7ab5006ae5c91540336a0943ad55c3914dd800f
Mikhail Buinouskiy <verybadprogrammer@gmail.com>
2022-03-25 18:13:21 +0300

    Please work

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env/node
+import path from "path";
+
 import log4js from "log4js";
 import app from "./app.js";
 import { bot } from "./routers/viber.js";
@@ -15,7 +17,7 @@ process.on("unhandledRejection", (reason, p) => {
 try {
   const server = app.listen(port, () => {
     logger.info(`Listening on port ${port}`);
-    bot.setWebhook(process.env.EXPOSE_URL + process.env.VIBER_WEBHOOK);
+    bot.setWebhook(path.join(process.env.BASE_URL, process.env.VIBER_WEBHOOK));
   });
   process.on("SIGTERM", () => {
     logger.warn("SIGTERM signal received: closing HTTP server");
