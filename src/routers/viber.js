a5fac3ce9b13d4450f10dec96e622e97c80b23bc
Mikhail Buinouskiy <verybadprogrammer@gmail.com>
2022-03-25 20:23:24 +0300

    Debugging

--- src/routers/viber.js
+++ src/routers/viber.js
@@ -19,8 +19,8 @@ function dateTimeToStr(date) {
 }
 
 async function sendNewRequestMessage(client) {
+  console.log(client);
   const request = await knex.table("requests").select().where("_id", client)[0];
-  console.log(client, request);
   const subs = await knex.table("viber_subs").select();
   subs.forEach(async (sub) => {
     // prettier-ignore
